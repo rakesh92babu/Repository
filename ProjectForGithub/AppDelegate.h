880691644015d05ba4eb75edef31b340f244154a
student <student@achiever-47.local>
2017-09-25 14:29:17 +0530

    Initial Commit

---
+++ ProjectForGithub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProjectForGithub
+//
+//  Created by student on 9/25/17.
+//  Copyright © 2017 Rakesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ProjectForGithub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProjectForGithub
+//
+//  Created by student on 9/25/17.
+//  Copyright © 2017 Rakesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
